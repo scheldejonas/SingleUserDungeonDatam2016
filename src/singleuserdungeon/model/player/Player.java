99404f04837ea526797533d2de6912ebbab2b5b4
coilx <coilx@DESKTOP-2EH328N.lan>
2016-05-31 10:39:31 +0200

    Startet on the player Class in Model

--- src/singleuserdungeon/model/player/Player.java
+++ src/singleuserdungeon/model/player/Player.java
@@ -10,5 +10,68 @@ package singleuserdungeon.model.player;
  * @author misk
  */
 public class Player {
+    private String name;
+    private int hitPoints;
+    private int health;
+    private int level;
+    private boolean hasHealingPotion;
+    private int weaponDamage;
+
+    public Player() {
+    }
+
+    
+    
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public boolean isHasHealingPotion() {
+        return hasHealingPotion;
+    }
+
+    public void setHasHealingPotion(boolean hasHealingPotion) {
+        this.hasHealingPotion = hasHealingPotion;
+    }
+
+    public int getWeaponDamage() {
+        return weaponDamage;
+    }
+
+    public void setWeaponDamage(int weaponDamage) {
+        this.weaponDamage = weaponDamage;
+    }
+    
+    
     
 }
