7b023a38ccdd77f385838c02dfb505907d6658f5
scheldejonas <scheldejonas@gmail.com>
2016-06-09 16:50:30 +0200

    Update - Single User Dungeon is cleaned

--- src/singleuserdungeon/control/SingeUserDungeon.java
+++ src/singleuserdungeon/control/SingeUserDungeon.java
@@ -25,17 +25,7 @@ public class SingeUserDungeon
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        BaseMonster monster = XmlMonsterParser.instance().GetMonster(0);
-        
-        BaseItem item = XmlItemParser.instance().getItem(0);
-        
-        BaseRoom room = XmlRoomParser.instance().getRoom(0);
-        
-        System.out.println(monster.toString());
-        System.out.println(item.toString());
-        System.out.println(room.toString());
-        
+                
         GameController.instance(); //Opening the game, the runner ends after this, therefore it is possible to start 2 games, with this Starter, by making a new instance variable in GameController
         
     }
